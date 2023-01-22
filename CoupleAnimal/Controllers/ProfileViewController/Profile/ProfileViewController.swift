5da5c73eaeee2a262ad373f4cdafe4edd2454b0c
Nikita <55090468+banger980@users.noreply.github.com>
2023-01-22 12:48:36 +0300

    CreatedPanelController

---
+++ CoupleAnimal/Controllers/ProfileViewController/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  CoupleAnimal
+//
+//  Created by Nikita on 18.01.23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+
+}
