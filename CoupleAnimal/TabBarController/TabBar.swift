327ba12eb45fda1fb1df011b6009ef77a2bdbe27
Nikita <55090468+banger980@users.noreply.github.com>
2023-01-22 14:44:41 +0300

    addPanelToController

--- CoupleAnimal/TabBarController/TabBar.swift
+++ CoupleAnimal/TabBarController/TabBar.swift
@@ -34,6 +34,7 @@ final class TabBarController: UITabBarController {
             $1.tabBarItem.title = dataSource[$0].title
             $1.tabBarItem.image = UIImage(systemName: dataSource[$0].iconName)
             tabBar.tintColor = .red
+            tabBar.backgroundColor = .white
         }
     }
     
