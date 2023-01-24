bc8948b9a75ed3707b1e22fde5780508b0149cfb
Nikita <55090468+banger980@users.noreply.github.com>
2023-01-24 14:48:39 +0300

    makeCustomOvalTabBar

--- CoupleAnimal/TabBarController/TabBar.swift
+++ CoupleAnimal/TabBarController/TabBar.swift
@@ -12,6 +12,7 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBar()
+        setupTabBarAppearance()
     }
 
     private func setupTabBar() {
@@ -33,14 +34,35 @@ final class TabBarController: UITabBarController {
         self.viewControllers?.enumerated().forEach {
             $1.tabBarItem.title = dataSource[$0].title
             $1.tabBarItem.image = UIImage(systemName: dataSource[$0].iconName)
-            tabBar.tintColor = .red
-            tabBar.backgroundColor = .white
+            //tabBar.tintColor = .red
+            //tabBar.backgroundColor = .white
         }
     }
     
     private func wrappedInNavigationController(with: UIViewController, title: Any?) -> UINavigationController {
             return UINavigationController(rootViewController: with)
     }
+    
+    private func setupTabBarAppearance() {
+        //для рисование кривых безье
+        let positionOnX: CGFloat = 10
+        let positionOnY: CGFloat = 14
+        let width = tabBar.bounds.width - positionOnX * 2
+        let height = tabBar.bounds.height + positionOnY * 2
+        
+        let roundLayer = CAShapeLayer()
+        
+        let bezierPath = UIBezierPath(roundedRect: CGRect(x: positionOnX, y: tabBar.bounds.minY - positionOnY, width: width, height: height), cornerRadius: height / 2)
+        
+        roundLayer.path = bezierPath.cgPath
+        tabBar.layer.insertSublayer(roundLayer, at: 0)
+        tabBar.itemWidth = width / 5
+        tabBar.itemPositioning = .centered
+        roundLayer.fillColor = UIColor.white.cgColor
+        tabBar.tintColor = .gray
+    }
+    
+    
     
 }
 
