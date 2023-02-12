7983e70fda938d47c303a26402885c1006b74a24
Nikita <55090468+banger980@users.noreply.github.com>
2023-02-13 02:31:02 +0300

    fixOverlapsTextField

--- CoupleAnimal/Controllers/FillingDataViewController/FillingDataViewController.swift
+++ CoupleAnimal/Controllers/FillingDataViewController/FillingDataViewController.swift
@@ -27,11 +27,25 @@ class FillingDataViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         petImage.isUserInteractionEnabled = true
         petImage.layer.cornerRadius = 25
         warningLabel.isHidden = true
         addGestures()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     
